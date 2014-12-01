23f60227ed544d5584a7dcba2e5208b8356ddc1e
ilya <ilya@SharkBay-Platform>
2014-12-01 12:35:25 +0300

    TariffServiceImpl tests

--- src/test/java/ru/tsystems/ecare/services/TariffServiceImplTest.java
+++ src/test/java/ru/tsystems/ecare/services/TariffServiceImplTest.java
@@ -1,30 +1,20 @@
 package ru.tsystems.ecare.services;
 
-import com.mysql.jdbc.Connection;
-import static org.junit.Assert.*;
-
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-import org.hibernate.SessionFactory;
-import org.junit.After;
 import org.junit.Before;
-
 import org.junit.Test;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-import org.springframework.test.jdbc.JdbcTestUtils;
 import ru.tsystems.ecare.persistence.dao.impl.ContractDAOImpl;
 import ru.tsystems.ecare.persistence.dao.impl.OptionDAOImpl;
 import ru.tsystems.ecare.persistence.dao.impl.TariffDAOImpl;
-
 import ru.tsystems.ecare.persistence.entities.Tariff;
 import ru.tsystems.ecare.services.impl.TariffServiceImpl;
 
@@ -32,35 +22,93 @@ import ru.tsystems.ecare.services.impl.TariffServiceImpl;
 public class TariffServiceImplTest extends AbstractJUnit4SpringContextTests {
 
     TariffServiceImpl tariffService;
+    ContractDAOImpl contractDAO;
+    OptionDAOImpl optionDAO;
+    TariffDAOImpl tariffDAO;
+    Tariff tariff;
+    Set<Tariff> tariffs;
 
     @Before
-    public void setUp() {
+    public final void setUp() {
+
         tariffService = new TariffServiceImpl();
-        tariffService.setContractDAO(mock(ContractDAOImpl.class));
-        tariffService.setOptionDAO(mock(OptionDAOImpl.class));
-        tariffService.setTariffDAO(mock(TariffDAOImpl.class));
+
+        contractDAO = mock(ContractDAOImpl.class);
+        tariffService.setContractDAO(contractDAO);
+
+        optionDAO = mock(OptionDAOImpl.class);
+        tariffService.setOptionDAO(optionDAO);
+
+        //setup tariffDAO mock
+        tariffDAO = mock(TariffDAOImpl.class);
+        tariffService.setTariffDAO(tariffDAO);
+
+        tariff = new Tariff("test-tariff", 0f);
+        tariffs = new HashSet<>();
+        //all tariffs
+        when(tariffDAO.all()).thenReturn(tariffs);
+        //add tariff
+        doAnswer(new TariffAnswer("add")).when(tariffDAO).add(tariff);
+        //delete tariff
+        doAnswer(new TariffAnswer("remove")).when(tariffDAO).remove(tariff);
+
+    }
+
+    @Test
+    public void testGetAvailableTariffs() {
+
+        tariffs.add(tariff);
+
+        assert (tariffService.getAvailableTariffs().contains(tariff));
     }
 
     @Test
     public void testCreateTariff() {
 
-        Tariff tariff = new Tariff("test-tariff", -3.0f);
-
         tariffService.createTariff(tariff);
+
+        assert (tariffService.getAvailableTariffs().contains(tariff));
+
     }
-//
-//    @Test
-//    public void testTasksForEmployees() {
-//        Employee steve = employeeDao.find(1L);
-//        Employee bill = employeeDao.find(2L);
-//
-//        assertEquals(2, timesheetService.tasksForEmployee(steve).size());
-//        assertEquals(1, timesheetService.tasksForEmployee(bill).size());
-//    }
-//
-//    @Test
-//    public void testTasksForManagers() {
-//        Manager eric = managerDao.find(1L);
-//        assertEquals(1, timesheetService.tasksForManager(eric).size());
-//    }
+
+    @Test
+    public void testDeleteTariff() {
+        tariffService.deleteTariff(tariff);
+        assert (!tariffService.getAvailableTariffs().contains(tariff));
+    }
+
+    @Test
+    public void testChangeTariff() {
+//        tariffService.
+        assert (true);
+    }
+
+    class TariffAnswer implements Answer<Object> {
+
+        private final String action;
+
+        public TariffAnswer(String action) {
+            this.action = action;
+        }
+
+        @Override
+        public Object answer(final InvocationOnMock invocation)
+                throws Throwable {
+            switch (action) {
+                case "remove":
+                    tariffs.remove(tariff);
+                    break;
+                case "add":
+                    tariffs.add(tariff);
+                    break;
+                default:
+                    break;
+            }
+
+            Object[] args = invocation.getArguments();
+            return "called with arguments: " + Arrays.toString(args);
+
+        }
+
+    }
 }
